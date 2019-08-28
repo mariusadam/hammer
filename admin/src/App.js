a7be0c574156f93e303e2a307ad44b727ca0ed92
Marius Adam <marius.adam@evozon.com>
2019-08-28 17:42:00 +0300

    Some updates

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,56 +1,57 @@
 import React from 'react';
-import { FunctionField, ImageField, ImageInput, RichTextField } from 'react-admin';
+import {FunctionField, ImageField, ImageInput, RichTextField} from 'react-admin';
 import RichTextInput from 'ra-input-rich-text';
-import { HydraAdmin } from '@api-platform/admin';
+import {HydraAdmin} from '@api-platform/admin';
 import parseHydraDocumentation from '@api-platform/api-doc-parser/lib/hydra/parseHydraDocumentation';
 
 const entrypoint = process.env.REACT_APP_API_ENTRYPOINT;
 
+const toBase64 = file => new Promise((resolve, reject) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = () => resolve(reader.result);
+  reader.onerror = error => reject(error);
+});
+
 const myApiDocumentationParser = entrypoint => parseHydraDocumentation(entrypoint)
-    .then( ({ api }) => {
-        const projects = api.resources.find(({name}) => 'projects' === name);
-        const description = projects.fields.find(f => 'description' === f.name);
+  .then(({api}) => {
+    const projects = api.resources.find(({name}) => 'projects' === name);
+    const description = projects.fields.find(f => 'description' === f.name);
 
-        description.field = props => (
-            <RichTextField {...props} source="description"/>
-        );
-        description.input = props => (
-            <RichTextInput {...props} source="description"/>
-        );
-        description.input.defaultProps = {
-            addField: true,
-            addLabel: true
-        };
+    description.field = props => (
+      <RichTextField {...props} source="description"/>
+    );
+    description.input = props => (
+      <RichTextInput {...props} source="description"/>
+    );
+    description.input.defaultProps = {
+      addField: true,
+      addLabel: true
+    };
 
-        const images = api.resources.find(({name}) => 'images' === name);
-        const contentUrl = images.fields.find(f => 'contentUrl' === f.name);
-        console.log(images.fields);
-        contentUrl.field = props => (
-            <ImageField {...props} source="contentUrl"/>
-        );
-        contentUrl.input = props => (
-            <ImageInput accept="image/*" key="contentUrl" multiple={false} source="contentUrl">
-                <ImageField source="contentUrl"/>
-            </ImageInput>
-        );
+    const images = api.resources.find(({name}) => 'images' === name);
+    const contentUrl = images.fields.find(f => 'contentUrl' === f.name);
+    const imageFile = images.fields.find(f => 'imageFile' === f.name);
+    contentUrl.field = props => (
+      <ImageField {...props} source="contentUrl"/>
+    );
 
-        // contentUrl.normalizeData = value => {
-        //     if (value && value.rawFile instanceof File) {
-        //         const body = new FormData();
-        //         body.append('file', value.rawFile);
-        //
-        //         return fetch(`${entrypoint}/images`, {body, method: 'POST', headers: {'Content-Type': 'multipart/form-data'}})
-        //             .then(response => response.json())
-        //             .then(image => {
-        //                 console.log(image);
-        //                 return image
-        //             });
-        //         r
-        //     }
-        // };
+    imageFile.input = props => (
+      <ImageInput accept="image/*" key="imageFasdile" multiple={false} source="imageFile">
+        <ImageField source="imageFileaaassadad"/>
+      </ImageInput>
+    );
 
-        return { api };
-    })
+    imageFile.normalizeData = value => {
+      if (value && value.rawFile instanceof File) {
+        return toBase64(value.rawFile);
+      }
+
+      return null;
+    };
+
+    return {api};
+  })
 ;
 
 export default (props) => <HydraAdmin apiDocumentationParser={myApiDocumentationParser} entrypoint={entrypoint}/>;
