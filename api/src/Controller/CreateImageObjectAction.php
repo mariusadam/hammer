b492327b752bfc2196bbfa04365cd65e84447789
Marius Adam <marius.adam@evozon.com>
2019-08-30 15:16:58 +0300

    Refactored Image create controller

--- api/src/Controller/CreateImageObjectAction.php
+++ api/src/Controller/CreateImageObjectAction.php
@@ -16,6 +16,7 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 
 final class CreateImageObjectAction
 {
+    private const FIELD_IMAGE_FILE = 'imageFile';
     private $denormalizer;
     private $decoder;
 
@@ -35,14 +36,45 @@ final class CreateImageObjectAction
         return $this->handleMultipartFormDataRequest($request);
     }
 
-    private function createMissingFileException(): \Exception
+    private function handleApplicationJsonRequest(Request $request): Image
     {
-        return new BadRequestHttpException('"imageFile" is required');
+        [$imageFile, $requestFields] = $this->extractImageFile($this->getDecodedBody($request));
+        $image = $this->denormalizeImage($requestFields);
+        $image->setImageFile($this->createUploadedFile($imageFile));
+
+        return $image;
+    }
+
+    private function createUploadedFile(string $base64EncodedFile): UploadedFile
+    {
+        // here we fake a file upload in order to leverage vich uploader bundle which checks for UploadedFile instances
+        [$prefix, $base64EncodedImage] = $this->extractImagePrefixAndContents($base64EncodedFile);
+        $extension = $this->getExtensionFromBase64Prefix($prefix);
+        $temporaryFilename = $this->createTemporaryImageFile();
+        $this->saveImageTo($temporaryFilename, $base64EncodedImage);
+        $originalName = $this->getOriginalName($temporaryFilename, $extension);
+
+        return new UploadedFile($temporaryFilename, $originalName, null, null, true);
+    }
+
+    private function getOriginalName(string $temporaryFilename, string $extension): string
+    {
+        return sprintf('%s.%s', pathinfo($temporaryFilename, PATHINFO_BASENAME), $extension);
+    }
+
+    private function extractImagePrefixAndContents(string $base64EncodedFile): array
+    {
+        $parts = explode(',', $base64EncodedFile);
+        if (count($parts) !== 2) {
+            throw $this->createUnsupportedImageFormatException();
+        }
+
+        return $parts;
     }
 
     private function handleMultipartFormDataRequest(Request $request): Image
     {
-        $uploadedFile = $request->files->get('imageFile');
+        $uploadedFile = $request->files->get(self::FIELD_IMAGE_FILE);
         if (!$uploadedFile instanceof UploadedFile) {
             throw $this->createMissingFileException();
         }
@@ -53,32 +85,15 @@ final class CreateImageObjectAction
         return $image;
     }
 
-    private function handleApplicationJsonRequest(Request $request): Image
+    private function extractImageFile(array $imageFields): array
     {
-        $imageFields = $this->getDecodedBody($request);
-        if (!isset($imageFields['imageFile'])) {
+        if (!isset($imageFields[self::FIELD_IMAGE_FILE])) {
             throw $this->createMissingFileException();
         }
+        $imageFile = $imageFields[self::FIELD_IMAGE_FILE];
+        unset($imageFields[self::FIELD_IMAGE_FILE]);
 
-        // here we fake a file upload in order to leverage vich uploader bundle which checks for UploadedFile instances
-        $temporaryFilename = tempnam(sys_get_temp_dir(), 'image-file-');
-        $parts = explode(',', $imageFields['imageFile']);
-        if (count($parts) !== 2) {
-            throw new UnsupportedMediaTypeHttpException('Image format is not supported');
-        }
-        [$prefix, $base64EncodedContents] = $parts;
-        $extension = $this->getExtensionFromBase64Prefix($prefix);
-        $originalName = pathinfo($temporaryFilename, PATHINFO_BASENAME);
-        file_put_contents($temporaryFilename, base64_decode($base64EncodedContents));
-        unset($imageFields['imageFile']);
-
-        $originalName = sprintf('%s.%s', $originalName, $extension);
-        $uploadedFile = new UploadedFile($temporaryFilename, $originalName, null, null, true);
-
-        $image = $this->denormalizeImage($imageFields);
-        $image->setImageFile($uploadedFile);
-
-        return $image;
+        return [$imageFile, $imageFields];
     }
 
     private function getExtensionFromBase64Prefix(string $prefix): string
@@ -89,7 +104,7 @@ final class CreateImageObjectAction
             case 'data:image/png;base64':
                 return 'png';
             default:
-                throw new UnsupportedMediaTypeHttpException('Image format is not supported');
+                throw $this->createUnsupportedImageFormatException();
         }
     }
 
@@ -108,5 +123,25 @@ final class CreateImageObjectAction
         $options = [JsonDecode::ASSOCIATIVE => true];
 
         return $this->decoder->decode($request->getContent(), JsonEncoder::FORMAT, $options);
+    }
+
+    private function createUnsupportedImageFormatException(): \Exception
+    {
+        return new UnsupportedMediaTypeHttpException('Image format is not supported');
+    }
+
+    private function createMissingFileException(): \Exception
+    {
+        return new BadRequestHttpException(sprintf('"%s" is required', self::FIELD_IMAGE_FILE));
+    }
+
+    private function createTemporaryImageFile(): string
+    {
+        return tempnam(sys_get_temp_dir(), 'image-file-');
+    }
+
+    private function saveImageTo(string $temporaryFilename, string $base64EncodedImage): void
+    {
+        file_put_contents($temporaryFilename, base64_decode($base64EncodedImage));
     }
 }
